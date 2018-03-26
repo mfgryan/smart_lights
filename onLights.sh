0b44fa0905574a5f2286b0552136c203379b5c51
mfgryan <ryanbhenao@gmail.com>
2018-03-25 20:18:18 -0700

    initial commit

---
+++ onLights.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "please give a light to switch"
+    exit 0
+fi
+
+USER=$(cat user)
+URL="http://192.168.0.8"
+
+curl -X "PUT" --data "{\"on\": true}" ${URL}/api/${USER}/lights/$1/state
+echo -e "\n"
